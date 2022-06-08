df555d123a48e820affbaa86b6b2d04b7b1304ae
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-08 00:47:40 +0000

    updated selector,deleted comment

--- rest-client/src/App.tsx
+++ rest-client/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import HeaderView from './views/HeaderView';
 import ChatView from './views/ChatView';
 import LoginView from './views/LoginView';
-import { useAppSelector } from './store/Dispatcher';
+import { useAppSelector } from './store/Selector';
 
 function App() {
   const userIsAuthenticated : boolean = useAppSelector((state) => state.authentication.authenticated)
--- rest-client/src/store/store.ts
+++ rest-client/src/store/store.ts
@@ -10,5 +10,4 @@ const store = configureStore({
 export default store
 
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
