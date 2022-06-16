730c51b5f2e2f456ac3ad19f2df26b70e151b3fe
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-16 18:51:41 +0000

    action is now synchronous

--- rest-client/src/store/chatActions.ts
+++ rest-client/src/store/chatActions.ts
@@ -1,9 +1,14 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk,createAction } from "@reduxjs/toolkit"
 import chat,{chatRequest} from "../services/chat"
+import { ChatState,AuthenticationState } from "./stateTypes"
+import Cookies from "js-cookie"
 
-export const sendMessageAction = createAsyncThunk("chat/SEND_MESSAGE",async(request : chatRequest) => {
-    let response = await chat(request)
-    let jsonResponse = await response?.json()
-    console.log(jsonResponse)
-    return jsonResponse
+export const sendMessageAction = createAction("chat/SEND_MESSAGE",function prepare(response){
+    console.log(response)
+    let payload : ChatState = {
+        message : ""
+    }
+    return {
+        payload: payload
+    }
 })
