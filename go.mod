e99adc1322bcb73e15172195125f84799327b353
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-22 02:58:13 +0000

    added server package to run chat api

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/mrpiggy97/restClient
 
 go 1.16
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/mrpiggy97/piggyhttp v1.0.5 // indirect
+	github.com/mrpiggy97/rest v1.0.1
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
+	"sync"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/mrpiggy97/rest/server"
 )
 
 type Server struct {
-	router *httprouter.Router
+	router    *httprouter.Router
+	apiServer *server.Server
 }
 
 func (server *Server) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
@@ -19,13 +22,19 @@ func (server *Server) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 
 func NewServer() *Server {
 	var router *httprouter.Router = httprouter.New()
-	router.ServeFiles("/*filepath", http.Dir("./rest-client/public"))
+	router.ServeFiles("/*filepath", http.Dir("./public"))
+	apiServer, _ := server.NewServer(context.Background())
 	return &Server{
-		router: router,
+		router:    router,
+		apiServer: apiServer,
 	}
 }
 
 func main() {
-	var appServer *Server = NewServer()
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", appServer))
+	//var appServer *Server = NewServer()
+	//log.Fatal(http.ListenAndServe("0.0.0.0:8000", appServer))
+	var waiter *sync.WaitGroup = new(sync.WaitGroup)
+	waiter.Add(1)
+	go server.Runserver()
+	waiter.Wait()
 }
