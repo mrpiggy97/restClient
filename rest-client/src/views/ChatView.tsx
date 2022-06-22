bb5d4240de69b054165251d4a9d1a88784442195
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-22 02:56:34 +0000

    eliminated websocket and its implementation,eliminatied function that returns a uuid, added effects to update state and start websocket connection

--- rest-client/src/views/ChatView.tsx
+++ rest-client/src/views/ChatView.tsx
@@ -5,29 +5,16 @@ import Message from "../components/Message"
 import {useAppSelector} from "../store/selector"
 import {useAppDispatch} from "../store/dispatcher"
 import {logoutAction} from "../store/authenticationActions"
-import WebSocketConnection from "../repository/WebSocket"
-import chat,{chatRequest} from "../services/chat"
-
-WebSocketConnection()
-
-type chatResponse = {
-    message : string
-}
-
-function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
+import chat,{chatRequest,chatResponse} from "../services/chat"
+import {sendMessageAction} from "../store/chatActions"
 
 export default function ChatView() : JSX.Element{
     const [chatMessage, setChatMessage] = useState("")
-    const currentMessage : string = useAppSelector((state) => state.chat.message)
-    const [messages,setMessages] = useState<Array<string>>([])
+    const currentMessage : chatResponse = useAppSelector((state) => state.chat)
+    const [messages,setMessages] = useState<Array<chatResponse>>([])
     let userIsAuthenticated = useAppSelector((state) => state.authentication.authenticated)
     let dispatcher = useAppDispatch()
+    let ws : WebSocket
 
     //update state
     const updateChatMessage = (event : ChangeEvent<HTMLInputElement>) : void => {
@@ -46,13 +33,57 @@ export default function ChatView() : JSX.Element{
             let request : chatRequest = {
                 message : chatMessage
             }
-            let response = await chat(request)
-            console.log(response)
+            await chat(request)
         } catch (error) {
             console.log(error)
         }
     }
+    // when component mounts we start websocket connection
+    useEffect(() => {
+        ws = new WebSocket("ws://localhost:5050/ws")
+        ws.onopen = function(){
+            console.log("connected to websocket")
+        }
+        ws.onmessage = function(event) : void{
+            console.log(event.data)
+            let decodedData : chatResponse = JSON.parse(event.data)
+            console.log(decodedData.message)
+            let response : chatResponse = {
+                message : decodedData.message,
+                uuid : decodedData.uuid
+            }
+            dispatcher(sendMessageAction(response))
+            console.log(event.data)
+        }
+        ws.onerror = function(event : Event) : void{
+            ws.close()
+            console.log(`error ${event}`)
+        }
 
+        ws.onclose = function(event):void{
+            console.log("websocket connection closed")
+        }
+        return () => {
+            ws.close()
+            console.log("websocket closed")
+        }
+    },[])
+
+    //when store is updated we push that value to messages array
+    useEffect(() => {
+        console.log(`current message ${currentMessage.message}`)
+        if (currentMessage.message.length > 0){
+            let arrayClone : Array<chatResponse> = messages.slice()
+            arrayClone.push(currentMessage)
+            console.log(messages)
+            setMessages(arrayClone)            
+        }
+    },[currentMessage])
+
+    const logMessages = () => {
+        console.log(messages)
+    }
+    
     if(userIsAuthenticated === true){
         return(
             <div id='chat-view'>
@@ -62,9 +93,10 @@ export default function ChatView() : JSX.Element{
                     <input type="text" id="message" onChange={updateChatMessage} />
                     <button type="submit">send</button>
                 </form>
+                <span onClick={logMessages}>click here to log messages</span>
                 <div id="messages">
                     {messages.map((message) => {
-                        return <Message message={message} key={uuid()} />
+                        return <Message message={message.message} key={message.uuid} />
                     })}
                 </div>
             </div>
