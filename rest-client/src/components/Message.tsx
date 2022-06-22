465064ecbe71f832bc3e7fb83bc6fdcac5f86e60
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-22 02:52:40 +0000

    updated component props type

--- rest-client/src/components/Message.tsx
+++ rest-client/src/components/Message.tsx
@@ -1,13 +1,12 @@
 import React from "react"
 
 type MessageProps = {
-    key : string
     message : string
 }
 
 export default function Message(props : MessageProps) : JSX.Element{
     return(
-        <div id={`${props.key}`}>
+        <div>
             <span>{props.message}</span>
         </div>
     )
