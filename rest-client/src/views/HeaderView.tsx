c909347d6702bc5365a708bdae3e7f6b9368b5dd
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-24 14:42:17 +0000

    updated text in header

--- rest-client/src/views/HeaderView.tsx
+++ rest-client/src/views/HeaderView.tsx
@@ -4,7 +4,7 @@ import "./css/Header.css"
 export default function HeaderView() : JSX.Element{
     return(
         <div id="header-view">
-            <p>header goes here</p>
+            <h2>Blocher chat</h2>
         </div>
     )
 }
