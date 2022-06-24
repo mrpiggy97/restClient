9325440d6c902eae02725f667490d3d607c3ea01
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-24 14:37:58 +0000

    added css

--- rest-client/src/components/Message.tsx
+++ rest-client/src/components/Message.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 
+import "./css/Message.css"
+
 type MessageProps = {
     message : string
 }
 
 export default function Message(props : MessageProps) : JSX.Element{
     return(
-        <div>
+        <div className="message">
             <span>{props.message}</span>
         </div>
     )
