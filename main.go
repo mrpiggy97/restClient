4c893be10cf08ccae520b17200bce7cbfc697823
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-11-17 20:15:27 +0000

    app now only serves client, it is expected that backend is available for connection

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"sync"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/mrpiggy97/rest/server"
 )
 
 type Server struct {
-	router    *httprouter.Router
-	apiServer *server.Server
+	router *httprouter.Router
 }
 
 func (server *Server) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
@@ -22,19 +19,13 @@ func (server *Server) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 
 func NewServer() *Server {
 	var router *httprouter.Router = httprouter.New()
-	router.ServeFiles("/*filepath", http.Dir("./public"))
-	apiServer, _ := server.NewServer(context.Background())
+	router.ServeFiles("/*filepath", http.Dir("./rest-client/public"))
 	return &Server{
-		router:    router,
-		apiServer: apiServer,
+		router: router,
 	}
 }
 
 func main() {
-	//var appServer *Server = NewServer()
-	//log.Fatal(http.ListenAndServe("0.0.0.0:8000", appServer))
-	var waiter *sync.WaitGroup = new(sync.WaitGroup)
-	waiter.Add(1)
-	go server.Runserver()
-	waiter.Wait()
+	var appServer *Server = NewServer()
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", appServer))
 }
